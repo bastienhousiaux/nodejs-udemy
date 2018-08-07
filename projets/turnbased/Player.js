ffaf493ddd0628254fa6275f3e8a5c0c72b530b5
Bastien Housiaux <bastienhousiaux@yahoo.fr>
2018-08-07 17:02:52 +0200

    end formation

--- projets/turnbased/Player.js
+++ projets/turnbased/Player.js
@@ -1,5 +1,8 @@
 class Player{
 
+    play(){
+        
+    }
 }
 
 module.exports=Player;
--- projets/turnbased/TurnBasedManager.js
+++ projets/turnbased/TurnBasedManager.js
@@ -1,7 +1,19 @@
-require("./Player.js");
+var Player=require("./Player.js");
 
 class TurnBasedManager{
     constructor(){
-        
+        this.players=[];
+        this.currentPlayerTurn=0;
+    }
+
+    addPlayer(player){
+        this.players.push(player);
+    }
+
+    nextPlayerTurn(){
+        this.nextPlayerTurn=(this.nextPlayerTurn<this.players.length)?++this.nextPlayerTurn:0;
+        this.players[i].play();
     }
 }
+
+module.exports=TurnBasedManager;
